a627011fb5bd360fc734121b58470bc29064ce3f
Mriyam Tamuli <mbtamuli@gmail.com>
2026-09-17 14:13:12 +0530

    Initialize Lume home page

---
+++ _config.ts
@@ -0,0 +1,7 @@
+import lume from "lume/mod.ts";
+
+const site = lume({
+  location: new URL("https://mriyam.dev"),
+});
+
+export default site;
---
+++ index.md
@@ -0,0 +1,7 @@
+---
+title: mriyam.dev
+---
+
+# mriyam.dev
+
+[Visit my digital garden](https://garden.mriyam.dev)
