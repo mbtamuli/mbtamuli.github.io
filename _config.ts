1a371f04692f604015d8ce7b064d7475ce4fff5c
Mriyam Tamuli <mbtamuli@gmail.com>
2026-09-17 14:57:09 +0530

    Apply Simple Me home theme

--- _config.ts
+++ _config.ts
@@ -1,7 +1,10 @@
 import lume from "lume/mod.ts";
+import me from "https://deno.land/x/lume_theme_simple_me/mod.ts";
 
 const site = lume({
   location: new URL("https://mriyam.dev"),
 });
+
+site.use(me());
 
 export default site;
--- index.md
+++
@@ -1,7 +0,0 @@
----
-title: mriyam.dev
----
-
-# mriyam.dev
-
-[Visit my digital garden](https://garden.mriyam.dev)
